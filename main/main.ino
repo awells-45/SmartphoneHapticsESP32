7214dd47c7804538eecec3f98de8fce9058955f4
Andrew Wells <56801781+awells-45@users.noreply.github.com>
2023-01-25 00:34:16 -0800

    Updated file

--- main/main.ino
+++ main/main.ino
@@ -1,40 +1,88 @@
+#include "BluetoothSerial.h"
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_IS31FL3731.h>
 
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBlue;
 Adafruit_IS31FL3731 matrix = Adafruit_IS31FL3731();
 
-uint8_t pixelArray[9][16];
+uint8_t hapticsArray[6][6];
+uint8_t serializedMatrix[36];
+uint8_t currSerialIndex;
 
-void initArray(){
-  for (int x = 0; x < 9; ++x)
+void initArrays(){
+  for (int x = 0; x < 6; ++x) // init haptics array
   {
-    for (int y = 0; y < 16; ++y)
+    for (int y = 0; y < 6; ++y)
     {
-      pixelArray[x][y] = int(x * y * 1.7) + 10; // diagonal brightness gradient as default pattern; range is 0-255
+      hapticsArray[x][y] = 0; // default to dark
+    }
+  }
+
+  for (int i = 0; i < 36; ++i) // init serial matrix
+  {
+    serializedMatrix[i] = 0;
+  }
+}
+
+void incrementSerialIndex()
+{
+   ++currSerialIndex;
+   if (currSerialIndex > 35)
+   {
+      currSerialIndex = 0;
+   }
+}
+
+void copySerialToMatrix()
+{
+  for (int i = 0; i <= 30; i = i + 6)
+  {
+    for (int x = 0; x < 6; ++x) 
+    {
+      int y = i / 6;
+      hapticsArray[x][y] = serializedMatrix[i + x];
+    }
+  }
+}
+
+void copyMatrixToPixelArray()
+{
+  matrix.setRotation(0); // draw matrix
+  for (int x = 6; x < 12; ++x)
+  {
+    for (int y = 2; y < 8; ++y)
+    {
+      matrix.drawPixel(x, y, hapticsArray[x - 6][y - 2]);
     }
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("ISSI manual animation test");
   if (! matrix.begin()) {
     Serial.println("IS31 not found");
     while (1);
   }
   Serial.println("IS31 Found!");
-  initArray();
+  initArrays();
+  currSerialIndex = 0;
   matrix.clear();
+  SerialBlue.begin("SmartphoneHaptics"); //device name
 }
 
 void loop() {
-  matrix.setRotation(0);
-  for (int x = 0; x < 9; ++x)
-  {
-    for (int y = 0; y < 16; ++y)
-    {
-      matrix.drawPixel(y, x, pixelArray[x][y]); // matrix is in a "portrait" orientation to match a smartphone screen
-    }
+  if (SerialBlue.available()) { // if there is new bluetooth data
+    uint8_t incomingValue = (uint8_t)SerialBlue.read();
+    
+    serializedMatrix[currSerialIndex] = incomingValue;
+    incrementSerialIndex();
+
+    copySerialToMatrix();
+    copyMatrixToPixelArray();
   }  
 }
